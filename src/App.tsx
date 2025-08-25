6f1e85de63bb4fd5771aae8a4ed84bf60117518f
ponlapoj1991 <ponlapoj.w@realsmart.co.th>
2025-08-26 00:20:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import SocialListeningDashboard from "./SocialListeningDashboard";
 import NotFound from "./pages/NotFound";
+import SettingsPage from "./pages/SettingsPage";
+import { AIProvider } from "./contexts/AIContext";
+import { DashboardProvider } from "./contexts/DashboardContext";
+
 
 const queryClient = new QueryClient();
 
@@ -14,11 +18,15 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AIProvider>
+          <DashboardProvider>
+            <Routes>
+              <Route path="/" element={<SocialListeningDashboard />} />
+              <Route path="/settings" element={<SettingsPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </DashboardProvider>
+        </AIProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
